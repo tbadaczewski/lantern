b619cfc878a2481707135ffa74e779c49e91d607
tbadaczewski <badaczewski@gmail.com>
2014-03-02 12:35:48 -0500

    Splash 5

--- js/app.js
+++ js/app.js
@@ -10,12 +10,16 @@ var lanternApp = angular.module('lanternApp', [
 lanternApp.run(function($rootScope, geolocation, geoencoder) {
     document.addEventListener('deviceready', function() {
         geolocation().then(function(position) {
-            $rootScope.position = position;
+            $rootScope.$apply(function() {
+                $rootScope.position = position;
 
-            geoencoder('latLng').then(function(address) {
-                $rootScope.address = address[0];
-                $rootScope.county = address[1];
-                $rootScope.state = address[2];
+                geoencoder('latLng').then(function(address) {
+                    $rootScope.$apply(function() {
+                        $rootScope.address = address[0];
+                        $rootScope.county = address[1];
+                        $rootScope.state = address[2];
+                    });
+                });
             });
         });
     });
