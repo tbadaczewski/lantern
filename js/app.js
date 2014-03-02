4f080c43b7b142f825bd142404b99785133e9771
tbadaczewski <badaczewski@gmail.com>
2014-03-02 12:30:05 -0500

    Device Ready Changes

--- js/app.js
+++ js/app.js
@@ -8,13 +8,15 @@ var lanternApp = angular.module('lanternApp', [
 ]);
 
 lanternApp.run(function($rootScope, geolocation, geoencoder) {
-    geolocation().then(function(position) {
-        $rootScope.position = position;
+    document.addEventListener('deviceready', function() {
+        geolocation().then(function(position) {
+            $rootScope.position = position;
 
-        geoencoder('latLng').then(function(address) {
-            $rootScope.address = address[0];
-            $rootScope.county = address[1];
-            $rootScope.state = address[2];
+            geoencoder('latLng').then(function(address) {
+                $rootScope.address = address[0];
+                $rootScope.county = address[1];
+                $rootScope.state = address[2];
+            });
         });
     });
 });
