806d4cd15d07816a8edc6607d03fa3c76c61cf6e
tbadaczewski <badaczewski@gmail.com>
2014-03-17 18:48:11 -0400

    Tips

--- js/app.js
+++ js/app.js
@@ -61,7 +61,7 @@ lanternApp.factory('geolocation', ['$q', '$rootScope', '$window',
     function ($q, $rootScope, $window) {
         return function () {
             var deferred = $q.defer();
-            var options = {maximumAge: 10000, timeout: 30000, enableHighAccuracy: true}
+            var options = {maximumAge: Infinity, timeout: 0, enableHighAccuracy: true}
             var onSuccess = function(position) {
                 alert(position.coords.latitude + ' - ' + position.coords.longitude);
 
