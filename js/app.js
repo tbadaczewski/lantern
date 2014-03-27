6dacf82910df2496714261d18e6fed6480b65d48
tbadaczewski <badaczewski@gmail.com>
2014-03-26 20:41:42 -0400

    3

--- js/app.js
+++ js/app.js
@@ -283,10 +283,10 @@ lanternApp.directive('googlemap', function($rootScope) {
             }
 
             scope.$watch('markers', function(newValue, oldValue) {
-                if (newValue !== oldValue) {
+                //if (newValue !== oldValue) {
                     scope.init();
                     alert("Watch");
-                }
+                //}
             }, true);
 
             scope.init();
