d08cdc4aed28269bdde8073ab8dee2c48a2452bd
tbadaczewski <badaczewski@gmail.com>
2014-03-27 22:30:56 -0400

    62

--- js/app.js
+++ js/app.js
@@ -310,7 +310,7 @@ lanternApp.directive('modaldialog', function($rootScope) {
 
             scope.toggleModal = function() {
                 if(scope.show === false) {
-                    //document.body.insertBefore(element[0], document.body.firstChild);
+                    document.body.insertBefore(element[0], document.body.firstChild);
                     scope.show = true;
                 } else {
                     scope.show = false;
