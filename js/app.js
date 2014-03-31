face5a95ab437c527dd279a3111fe460c655bc82
tbadaczewski <badaczewski@gmail.com>
2014-03-30 21:41:26 -0400

    51

--- js/app.js
+++ js/app.js
@@ -337,6 +337,7 @@ lanternApp.directive('contentframe', function() {
             scope.history = [scope.frame.contentWindow.location.pathname];
 
             scope.frame.onload = function() {
+                this.contentWindow.document.body.scrolling = "no";
                 this.height = this.contentWindow.document.body.offsetHeight + "px";
 
                 if(this.contentWindow.location.pathname != scope.history[scope.index]) {
