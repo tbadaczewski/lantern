2c314b49d874428ade23e5852747ee5966346ddd
tbadaczewski <badaczewski@gmail.com>
2014-03-30 20:24:05 -0400

    44

--- js/app.js
+++ js/app.js
@@ -330,7 +330,7 @@ lanternApp.directive('contentframe', function() {
             src: '@',
             class: '@'
         },
-        template: "<div id='{{id}}'><a href='#/'>Main</a><button id='back' type='button' ng-click='back()'>Back</button><button id='forward' type='button' ng-click='forward()'>Forward</button><iframe src='{{src}}' width='100%' height='100%'>kjjkjkj</iframe></div>",
+        template: "<div id='{{id}}'><a href='#/'>Main</a><button id='back' type='button' ng-click='back()'>Back</button><button id='forward' type='button' ng-click='forward()'>Forward</button><iframe name='content' src='{{src}}' width='300' height='300'>kjjkjkj</iframe></div>",
         link: function (scope, element, attrs) {
             scope.index = 0;
             scope.frame = element[0].childNodes[3];
