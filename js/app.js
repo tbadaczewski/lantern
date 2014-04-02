f06415ee71171b77119c3c7577a5b758f0f4649a
tbadaczewski <badaczewski@gmail.com>
2014-04-02 14:49:05 -0400

    129

--- js/app.js
+++ js/app.js
@@ -74,16 +74,12 @@ lanternApp.factory('geolocation', ['$q', '$rootScope', '$window',
             var options = { maximumAge: 30000, timeout: 30000, enableHighAccuracy: false }
             
             function onSuccess(position) {
-                alert(position.coords.latitude);
-
                 //$rootScope.$apply(function () {
                     deferred.resolve(position);
                 //});
             }
 
             function onError(error) {
-                alert(error.message);
-
                 //$rootScope.$apply(function () {
                     deferred.resolve($rootScope.position);
                 //});
