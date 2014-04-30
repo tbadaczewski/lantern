67098793d11d7fce4d8492d0c9ba2f83ab70cede
tbadaczewski <badaczewski@gmail.com>
2014-04-30 14:25:21 -0400

    350

--- js/controllers.js
+++ js/controllers.js
@@ -88,10 +88,10 @@ lanternControllers.controller('MainCtrl', ['$scope', '$rootScope', '$http', '$wi
 		}
 
 		$scope.$watch("uri", function(newValue, oldValue) {
-			if($scope.uri != "") {
+			if (newValue !== oldValue) {
 				$window.plugins.socialsharing.share($rootScope.address + " #powerlinedown", null, $scope.uri, null, function(e) { alert("Success: " + e); }, function(e) { alert("Error: " + e); });
 			}
-		},true);
+		}, true);
     	
     	$scope.camera = function($event) {
     		$event.preventDefault();
