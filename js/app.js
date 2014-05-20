3c9a8e224fbbec1d4d05c984d629ae9cb68229d3
tbadaczewski <badaczewski@gmail.com>
2014-05-20 14:59:41 -0400

    594

--- js/app.js
+++ js/app.js
@@ -157,6 +157,7 @@ lanternApp.factory('geolocation', ['$q',
             var options = { maximumAge: 30000, timeout: 30000, enableHighAccuracy: false }
             
             function onSuccess(position) {
+                navigator.notification.alert('There was a problem locating your position, please manually enter your city, state or zipcode.', null, 'Failed to Locate Position', 'Close'); 
                 deferred.resolve(position);
             }
 
