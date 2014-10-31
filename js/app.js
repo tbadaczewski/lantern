843ea82e2736cd0905acddad08f39d77c043b8cd
tbadaczewski <badaczewski@gmail.com>
2014-10-30 20:04:06 -0400

    Moved location

--- js/app.js
+++ js/app.js
@@ -33,15 +33,15 @@ lanternApp.run(function($rootScope, $http, geolocation, geoencoder, loadstations
 
         geolocation().then(function(position) {
             $rootScope.position = position;
-            
-            loadstations().then(function(stations) {
-                $rootScope.stations = stations;
-            });
 
             geoencoder('latLng').then(function(address) {
                 $rootScope.address = address[0];
                 $rootScope.county = address[1];
                 $rootScope.state = address[2];
+
+                loadstations().then(function(stations) {
+                    $rootScope.stations = stations;
+                });
 
                 loadoutages().then(function(outages) {
                     $rootScope.outages = outages;
@@ -171,7 +171,7 @@ lanternApp.factory('geolocation', ['$q', '$window',
             }
 
             function onError(error) {
-                //$window.navigator.notification.alert('There was a problem locating your position, please manually enter your city, state or zipcode.', null, 'Failed to Locate Position', 'Close');
+                $window.navigator.notification.alert('There was a problem locating your position, please manually enter your city, state or zipcode.', null, 'Failed to Locate Position', 'Close');
                 deferred.resolve(null);
             }
 
