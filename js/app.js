97df8110354e1e3f9bc2b3d5d486b24bca702c58
tbadaczewski <badaczewski@gmail.com>
2014-11-13 13:15:12 -0500

    vxcvxcv

--- js/app.js
+++ js/app.js
@@ -531,7 +531,7 @@ lanternApp.directive('outageframe', function($http, $sce) {
         template: "<iframe ng-transclude></iframe>",
         link: function (scope, element, attrs) {
             element.bind("load", function(e) {
-                angular.element(element).attr('height', (element[0].clientHeight - 100) + "px");
+                angular.element(element).attr('height', (element[0].clientHeight - 140));
                 scope.$emit('loaded', true);
             });
         }
